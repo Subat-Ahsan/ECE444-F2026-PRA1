9aa74346a7ba5a0f21bac09cb60804b09052e0cb
Suabt Ahsan <subat.ahsan18@gmail.com>
2026-09-17 04:43:05 -0400

    Utils code

---
+++ utils.py
@@ -0,0 +1,21 @@
+class Utils:
+    def reversed(number: int) -> int:
+        if not isinstance(number, int):
+            raise TypeError("value must be an integer")
+
+        is_negative = number < 0
+
+        reversed_number = 0
+        number = abs(number)
+        while (number):
+            reversed_number = reversed_number * 10
+            reversed_number += number % 10 
+            number = number // 10
+
+        return -reversed_number if is_negative else reversed_number
+        
+    def formatter(number: int) -> tuple[str, str]:
+        if not isinstance(number, int):
+            raise TypeError("value must be an integer")
+
+        return bin(number), oct(number)
